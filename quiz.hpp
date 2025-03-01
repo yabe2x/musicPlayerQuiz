b3d97a64bca262bc2492e2979d44bcf2130367f5
Nathan Megersa <nhezi@Nathans-MacBook-Pro-4.local>
2025-03-01 12:06:58 -0700

    done

--- quiz.hpp
+++ quiz.hpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+
+using namespace std;
+
+
+
+
+class quiz
+{
+private:
+    string answer;
+    string artist;
+
+    
+public:
+    quiz(/* args */);
+    ~quiz();
+};
+
+quiz::quiz(/* args */)
+{
+}
+
+quiz::~quiz()
+{
+}
 
