9d6e978a123fbad8f2e05cc00974306b2cbe6bc8
Nathan Megersa <nhezi@Nathans-MacBook-Pro-4.local>
2025-03-01 14:11:49 -0700

    third

--- quiz.cpp
+++ quiz.cpp
@@ -17,13 +17,15 @@ private:
     Song *head;
     int highScores[10];
     int highScoresIndex = 0;
+    int totalNumberOfSongs = 0;
+    Song *choices[4];
 
     
 public:
     quiz();
     ~quiz();
     bool displayAnswers(Song *song); //after song is played, gives user multiple choice options to choose from and makes sure user input is valid. Returns true if user answer is correct, returns false otherwise
-    string sortMC(string answer); //sorts answers out, randomly having answer as A, B, C, or D
+    string sortMC(); //sorts answers out, randomly having answer as A, B, C, or D
     void readInSongs(fstream file); //reads in file of songs and creates a linked list out of all the songs
     Song returnSong(); // randomly selects a song from the linked list of songs for the user to guess the artist based off the sound clip
     
@@ -49,16 +51,35 @@ quiz::~quiz()
     }
 }
 
-string sortMC(string answer){
+string sortMC(){
+    bool validInput = false;
+    char userAnswer;
+    while(!validInput){
+        
+        
+        
+        if(userAnswer == 'A' || userAnswer == 'B' || userAnswer == 'C' || userAnswer == 'D'){
+            validInput = true;
+        }
+        
+    }
 
 }
 
 bool quiz::displayAnswers(Song *song){
     //play sound clip of song, then ask who is the artist and display the answer choices
-    bool validInput = false;
-    while(!validInput){
-
     
-        sortMC(song->artist);
-    }
+
+        cout << "Who is the artist of this song?" << endl;
+        cout << endl;
+        artist = song->artist;
+        answer = sortMC();
+        if(answer == artist){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    
 }
--- quiz.hpp
+++ quiz.hpp
@@ -22,16 +22,17 @@ private:
     Song *head;
     int highScores[10];
     int highScoresIndex = 0;
+    Song *choices[4];
 
     
 public:
     quiz();
     ~quiz();
     bool displayAnswers(Song *song); //after song is played, gives user multiple choice options to choose from and makes sure user input is valid. Returns true if user answer is correct, returns false otherwise
-    void sortMC(string answer); //sorts answers out, randomly having answer as A, B, C, or D
+    string sortMC(); //sorts answers out, randomly having answer as A, B, C, or D
     void readInSongs(fstream file); //reads in file of songs and creates a linked list out of all the songs
     Song returnSong(); // randomly selects a song from the linked list of songs for the user to guess the artist based off the sound clip
-    
+
     
 };
 
